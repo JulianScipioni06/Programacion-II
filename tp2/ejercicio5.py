ba3e614f11776fc1e12dfbd219cbcc7a9e2f48a1
JulianScipioni06 <scipionijulian@gmail.com>
2025-08-28 13:14:41 -0300

    tp2 - Ejercicio5

--- tp2/ejercicio5.py
+++ tp2/ejercicio5.py
@@ -1,22 +1,44 @@
+def PedirEntero(mensaje:str) -> int: 
+    bandera = True
+    while bandera:
+        try:
+            numero = int(input(mensaje))
+            if numero > 0:
+                bandera = False
+            else:
+                print("Error: El numero debe ser positivo.")
+        except ValueError:
+            print("ERROR! Ingrese un numero entero positivo: ")
+    return numero
+
+def PedirNota(mensaje:str) -> int: 
+    bandera = True
+    while bandera:
+        try:
+            numero = int(input(mensaje))
+            if numero > 0 and numero <= 100:
+                bandera = False
+            else:
+                print("Error: El numero debe ser positivo.")
+        except ValueError:
+            print("ERROR! Ingrese un numero entero positivo: ")
+    return numero
+
 alumnos = []
 
-cantAlumnos = int(input("Cuantos alumnos vas a cargar? "))
+cantAlumnos = PedirEntero("Cantidad de Alumnos a Ingresar: ")
 
 for i in range(cantAlumnos):
-    nombre = input("Ingrese nombre del alumno: ")
-    nota = float(input(f"Ingrese nota de {nombre}: "))
+    print("Datos del Alumno")
+    nombre = input("Ingrese nombre: ")
+    nota = PedirNota(f"Nota de {nombre}: ")
     print()
     
-    alumnos.append([nombre,nota])
+    alumnos.append({
+        "nombre": nombre,
+        "nota": nota
+    })
 
-resultados = []
-
-# for i in range(0,len(alumnos)):
-#     if alumnos[0,i] >= 40:
-#         resultados[i] = "Aprobado"
-#     else:
-#         resultados[i] = "Desaprobado"
-
-print(alumnos[0])
-print(alumnos[nombre])
-
+print("Resultados del Parcial")
+for alumno in alumnos:
+    print(f"Alumno: {alumno["nombre"]} / Nota: {alumno["nota"]} /Resultado: {"Aprobado" if alumno["nota"] >= 40 else "Desaprobado"}")
