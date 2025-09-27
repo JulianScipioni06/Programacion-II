f99f61c9f8e1097430b014af694fed3cd3a25c3c
Julian Scipioni <167777190+JulianScipioni06@users.noreply.github.com>
2025-09-27 11:32:55 -0300

    eje3-tp4

--- tp4/eje3/vinoteca.py
+++ tp4/eje3/vinoteca.py
@@ -4,13 +4,13 @@ class Vinoteca:
     
     #Constructor
     def __init__(self, cantJugos:int = 5000, cantBlancos: int = 5000, cantTintosJovenes: int = 5000, cantTintosAnejados: int = 5000):
-        if not isinstance(cantJugos,int) or cantJugos > 5000 or cantJugos < 0:
+        if not isinstance(cantJugos,int) or cantJugos > Vinoteca.__CAPACIDAD_SECCION or cantJugos < 0:
             raise ValueError("Cantidad de Jugos debe ser un Entero Positivo Menor que 5000.")
-        if not isinstance(cantBlancos,int) or cantBlancos > 5000 or cantBlancos < 0:
+        if not isinstance(cantBlancos,int) or cantBlancos > Vinoteca.__CAPACIDAD_SECCION or cantBlancos < 0:
             raise ValueError("Cantidad de Vinos Blancos debe ser un Entero Positivo Menor que 5000.")
-        if not isinstance(cantTintosJovenes,int) or cantTintosJovenes > 5000 or cantTintosJovenes < 0:
+        if not isinstance(cantTintosJovenes,int) or cantTintosJovenes > Vinoteca.__CAPACIDAD_SECCION or cantTintosJovenes < 0:
             raise ValueError("Cantidad de Vinos Tintos Jovenes debe ser un Entero Positivo Menor que 5000.")
-        if not isinstance(cantTintosAnejados,int) or cantTintosAnejados > 5000 or cantTintosAnejados < 0:
+        if not isinstance(cantTintosAnejados,int) or cantTintosAnejados > Vinoteca.__CAPACIDAD_SECCION or cantTintosAnejados < 0:
             raise ValueError("Cantidad de Vinos Tintos Añejados debe ser un Entero Positivo Menor que 5000.")
         
         self.__cantJugos = cantJugos
@@ -23,36 +23,36 @@ class Vinoteca:
         """Repone la cantidad de jugos disponibles. Requiere que cantJugos < 5000"""
         if not isinstance(cantidad,int) or cantidad < 0:
             raise ValueError("Cantidad debe ser Un Entero Positivo")
-        if self.__cantJugos + cantidad > 5000:
+        if self.__cantJugos + cantidad > Vinoteca.__CAPACIDAD_SECCION:
             raise ValueError("No se puede reponer porque supera la Capacidad Maxima(5000).")
-        if self.__cantJugos + cantidad <= 5000:
+        if self.__cantJugos + cantidad <= Vinoteca.__CAPACIDAD_SECCION:
             self.__cantJugos += cantidad
     
     def ReponerVinosBlancos(self,cantidad:int):
         """Repone la cantidad de Vinos Blancos disponibles. Requiere que cantBlancos < 5000"""
         if not isinstance(cantidad,int) or cantidad < 0:
             raise ValueError("Cantidad debe ser Un Entero Positivo")
-        if self.__cantBlancos + cantidad > 5000:
+        if self.__cantBlancos + cantidad > Vinoteca.__CAPACIDAD_SECCION:
             raise ValueError("No se puede reponer porque supera la Capacidad Maxima(5000).")
-        if self.__cantBlancos + cantidad <= 5000:
+        if self.__cantBlancos + cantidad <= Vinoteca.__CAPACIDAD_SECCION:
             self.__cantBlancos += cantidad 
     
     def ReponerVinosTintosJovenes(self,cantidad:int):
         """Repone la cantidad de Tintos Jovenes disponibles. Requiere que CantJugos < 5000"""
         if not isinstance(cantidad,int) or cantidad < 0:
             raise ValueError("Cantidad debe ser Un Entero Positivo")
-        if self.__cantTintosJovenes + cantidad > 5000:
+        if self.__cantTintosJovenes + cantidad > Vinoteca.__CAPACIDAD_SECCION:
             raise ValueError("No se puede reponer porque supera la Capacidad Maxima(5000).")
-        if self.__cantTintosJovenes + cantidad <= 5000:
+        if self.__cantTintosJovenes + cantidad <= Vinoteca.__CAPACIDAD_SECCION:
             self.__cantTintosJovenes += cantidad  
     
     def ReponerVinosTintosAnejados(self,cantidad:int):
         """Repone la cantidad de Tintos Anejados disponibles. Requiere que CantJugos < 5000"""
         if not isinstance(cantidad,int) or cantidad < 0:
             raise ValueError("Cantidad debe ser Un Entero Positivo")
-        if self.__cantTintosAnejados + cantidad > 5000:
+        if self.__cantTintosAnejados + cantidad > Vinoteca.__CAPACIDAD_SECCION:
             raise ValueError("No se puede reponer porque supera la Capacidad Maxima(5000).")
-        if self.__cantTintosAnejados + cantidad <= 5000:
+        if self.__cantTintosAnejados + cantidad <= Vinoteca.__CAPACIDAD_SECCION:
             self.__cantTintosAnejados += cantidad
     
     def VenderJugos(self,unidades:int):
@@ -118,4 +118,4 @@ class Vinoteca:
         return self.__cantTintosAnejados
     
     def __str__(self)->str:
-        return f"- Legajo: {self.__legajo} \n- Horas Trabajadas: {self.__horasTrabajadasMes} \n- Valor Hora: {self.__valorHora}"
+        return f"Cantidad en Stock: \n - Jugos Sin alcohol: {self.__cantJugos} \n - Vinos Blancos: {self.__cantBlancos} \n - Vinos Tintos Jovenes: {self.__cantTintosJovenes} \n - Vinos Tintos Anejados: {self.__cantTintosAnejados} \n"
